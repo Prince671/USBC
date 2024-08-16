1d799976916a98f147db988bcf1333a412f3db13
Prince soni <157681120+Prince671@users.noreply.github.com>
2024-08-16 19:36:29 +0530

    Create asgi.py

---
+++ asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for interest_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from django.urls import path
+from interest_app import consumers
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interest_project.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            [
+                path("ws/chat/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
+            ]
+        )
+    ),
+})
